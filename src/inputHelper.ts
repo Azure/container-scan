0cf73b8496e0707fde699b95e7396423ec890c6f
shigupt202 <57620559+shigupt202@users.noreply.github.com>
2020-04-02 16:59:30 +0530

    Updating input names and descriptions (#9)

--- src/inputHelper.ts
+++ src/inputHelper.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core';
 
 export const imageName = core.getInput("image-name");
-export const githubToken = core.getInput("github-token");
+export const githubToken = core.getInput("token");
 export const username = core.getInput("username");
 export const password = core.getInput("password");
 export const severityThreshold = core.getInput("severity-threshold");
-export const addCISChecks = core.getInput("add-CIS-checks");
+export const runQualityChecks = core.getInput("run-quality-checks");
 
-export function isCisChecksEnabled(): boolean {
-    return addCISChecks.toLowerCase() === "true";
+export function isRunQualityChecksEnabled(): boolean {
+    return runQualityChecks.toLowerCase() === "true";
 }
