105e988c3d7b5de4fe1cadf9f89212c55873abec
shigupt202 <57620559+shigupt202@users.noreply.github.com>
2020-04-28 22:45:55 +0530

    Adding input validation

--- src/inputHelper.ts
+++ src/inputHelper.ts
@@ -10,3 +10,10 @@ export const runQualityChecks = core.getInput("run-quality-checks");
 export function isRunQualityChecksEnabled(): boolean {
     return runQualityChecks.toLowerCase() === "true";
 }
+
+export function validateRequiredInputs() {
+    if (!imageName)
+        throw new Error("Could not find required input: image-name");
+    if (!githubToken)
+        throw new Error("'token' input is not supplied. Set it to a PAT/GITHUB_TOKEN");
+}
