775cdb319b77245d3088dc69d4edaab522dd76b4
ajinkya599 <ajinkya599@github.com>
2020-05-08 02:21:59 +0530

    Refactoring as per review comments

--- src/githubClient.ts
+++ src/githubClient.ts
@@ -8,35 +8,23 @@ export class GitHubClient {
         this._token = token;
     }
 
-    public async createCheckRunWithFallback(checkRunThroughAppPayload: any, checkRunPayload: any): Promise<void> {
+    public async createScanResult(payload: any): Promise<any> {
         const checkRunUrl = `https://api.github.com/repos/${this._repository}/container-scanning/check-run`;
         const webRequest = new WebRequest();
         webRequest.method = "POST";
         webRequest.uri = checkRunUrl;
-        webRequest.body = JSON.stringify(checkRunThroughAppPayload);
+        webRequest.body = JSON.stringify(payload);
         webRequest.headers = {
             Authorization: `Bearer ${this._token}`
         };
 
-        console.log(`Creating check run. image_name: ${checkRunThroughAppPayload['image_name']}, head_sha: ${checkRunThroughAppPayload['head_sha']}`);
+        console.log(`Creating scan result. image_name: ${payload['image_name']}, head_sha: ${payload['head_sha']}`);
         const response: WebResponse = await sendRequest(webRequest);
         core.debug(util.format('Response from scanitizer app:\n', response.body));
-        if (response.statusCode == StatusCodes.UNPROCESSABLE_ENTITY
-            && response.body
-            && response.body.message
-            && response.body.message.error_code === 'APP_NOT_INSTALLED') {
-            console.log('Looks like the scanitizer app is not installed on the repo. Falling back to check run creation through GitHub actions app...');
-            await this.createCheckRun(checkRunPayload);
-        }
-        else if (response.statusCode != StatusCodes.OK) {
-            throw Error(`Statuscode: ${response.statusCode}, StatusMessage: ${response.statusMessage}, head_sha: ${checkRunThroughAppPayload['head_sha']}`);
-        }
-        else {
-            console.log(`Created check run through app. Url: ${response.body['check_run']['html_url']}`);
-        }
+        return Promise.resolve(response);
     }
 
-    private async createCheckRun(payload: any): Promise<void> {
+    public async createCheckRun(payload: any): Promise<void> {
         const checkRunUrl = `https://api.github.com/repos/${this._repository}/check-runs`;
         const webRequest = new WebRequest();
         webRequest.method = "POST";
