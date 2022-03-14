211e2ec398ef61f583e77d4dd1e37c6e957ac894
Randi Øyri <randi.oyri@digdir.no>
2022-03-14 12:18:03 +0100

    Specify Trivy version in workflow file (version 2) (#130)
    * specify trivy version
    
    * Trivy version logging
    
    * Fix wrongly assigned variable for trivyversion.
    
    * Updated readme with new parameter.
    
    * Rename variable for Trivy version.
    
    * Build JS
    
    * Update src/trivyHelper.ts
    
    Co-authored-by: Koushik Dey <57134947+koushdey@users.noreply.github.com>
    
    * build trivyhelper.js - change debug message
    
    * Fix failing tests and add new test
    
    Co-authored-by: Koushik Dey <57134947+koushdey@users.noreply.github.com>

--- __tests__/trivyHelper.test.ts
+++ __tests__/trivyHelper.test.ts
@@ -1,24 +1,36 @@
+
 jest.mock('fs');
 jest.mock('os');
+
+
 
 describe('Run Trivy', () => {
     const mockedFs = require('fs');
     const mockedOs = require('os');
-    const mockedToolCache = require('@actions/tool-cache');
-    const mockedFileHelper = require('../src/fileHelper');
-    mockedFileHelper.getContainerScanDirectory = jest.fn().mockImplementation(() =>{
-        return 'test/_temp/containerscan_123';
-    });
 
-    let mockFile = {
-        'releaseDownloadedPath': JSON.stringify({ tag_name: 'v1.1.1' })
-    };
-    let cachedTools = {
-        'trivy': true,
-        'dockle': true
-    };
-    mockedFs.__setMockFiles(mockFile);
+    function setupFileMock() {
+        const mockedFileHelper = require('../src/fileHelper');
+        mockedFileHelper.getContainerScanDirectory = jest.fn().mockImplementation(() => {
+            return 'test/_temp/containerscan_123';
+        });
+
+        return {
+            'releaseDownloadedPath': JSON.stringify({tag_name: 'v1.1.1'})
+        };
+    }
+    mockedFs.__setMockFiles(setupFileMock());
+
+    function toolCache() {
+        const mockedToolCache = require('@actions/tool-cache');
+        let cachedTools = {
+            'trivy': true,
+            'dockle': true
+        };
+        return {mockedToolCache, cachedTools};
+    }
+    let {mockedToolCache, cachedTools} = toolCache();
     mockedToolCache.__setToolCached(cachedTools);
+
 
     afterAll(() => {
         jest.clearAllMocks();
@@ -36,7 +48,7 @@ describe('Run Trivy', () => {
         expect(mockedToolCache.downloadTool).toHaveBeenCalledTimes(1);
     });
 
-    test('Trivy binaries are not present in the cache', async () => {
+   test('Trivy binaries are not present in the cache', async () => {
         cachedTools['trivy'] = false;
         mockedToolCache.__setToolCached(cachedTools);
         const runner = require('../src/trivyHelper');
@@ -48,4 +60,32 @@ describe('Run Trivy', () => {
         expect(mockedToolCache.find).toHaveReturnedWith(undefined);
         expect(mockedToolCache.downloadTool).toHaveBeenCalledTimes(2);
     });
+
+    test('Trivy binaries are not present in the cache and not use latest version', async () => {
+        jest.resetModules();
+        const mockedFs = require('fs');
+        const mockedOs = require('os');
+        mockedFs.__setMockFiles(setupFileMock());
+        let {mockedToolCache, cachedTools} = toolCache();
+        cachedTools['trivy'] = false;
+        mockedToolCache.__setToolCached(cachedTools);
+        jest.mock('../src/inputHelper', () => {
+            const inputHelper = jest.requireActual('../src/inputHelper');
+            return {
+                __esModule: true,
+                ...inputHelper,
+                trivyVersion: '0.23.0',
+            };
+        });
+
+        const runner = require('../src/trivyHelper');
+        const trivyResult = await runner.runTrivy();
+        expect(trivyResult).toHaveProperty('status', 0);
+        expect(trivyResult).toHaveProperty('timestamp');
+        expect(mockedOs.type).toHaveBeenCalledTimes(1);
+        expect(mockedToolCache.find).toHaveReturnedWith(undefined);
+        expect(mockedToolCache.downloadTool).toHaveBeenCalledTimes(1);
+    });
+
+
 });
